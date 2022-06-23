b69277cf044f5556a22e2e317e51fb57bcd2b088
ktrung1709 <75299342+ktrung1709@users.noreply.github.com>
2022-06-23 18:33:27 +0700

    Update UniformCostSearch.py

--- SearchAlgos/UniformCostSearch.py
+++ SearchAlgos/UniformCostSearch.py
@@ -5,8 +5,7 @@ sys.path.append(str(sys.path[0]) + '\\Visualization')
 from VisualMienBac import printMap
 
 def UCS(start_city, end_city, city_map):
-    time_complexity = 0
-    space_complexity = 0
+    
     if start_city not in city_map:
         raise TypeError(str(start_city) + ' not found in graph !')
         return
@@ -18,34 +17,44 @@ def UCS(start_city, end_city, city_map):
         print('Best route: ')
         return
 
+    time_space = 1
+    cur_city = start_city
     queue = Q.PriorityQueue()
+    cities_list = [start_city]
+    cost = 0
+    visited = [start_city]
+
     # Initial queue
-    queue.put((0, [start_city]))
- 
+    queue.put((0, cities_list))
+
     while not queue.empty():
 
-        # Get path with smallest cost
+        for neighbor in city_map[cur_city]:
+            if neighbor not in visited:
+                temp = cities_list[:]
+                temp.append(neighbor)
+                time_space = time_space + 1
+                queue.put((cost + city_map[cur_city][neighbor], temp))
+        # Pop the top priority item out of the PriorityQueue
         node = queue.get()
-        time_complexity = time_complexity + 1
-        # Get last city in path 
-        current_city = node[1][len(node[1]) - 1]
+        # Get the cost, cities_list and last_city
         cost = node[0]
+        cities_list = node[1]
+        last_city = cities_list[len(cities_list) - 1]
 
-        if end_city in node[1]:
-            print("Time complexity: " + str(time_complexity))
-            print("Space complexity: " + str(space_complexity))
-            print("Total distance = " + str(node[0]))
-            print("Path found: " + str(node[1]))
-
-            printMap(node[1])
+        if end_city == last_city:
             break
+        
+        visited.append(last_city)
+        cur_city = last_city
 
-        # Get smallest cost
+    result = []
+    for item in cities_list:
+        item_delete_space = item.replace(" ","")
+        result.append(item_delete_space)
 
-        for neighbor in city_map[current_city]:
-
-            temp = node[1][:]
-            temp.append(neighbor)
-            if len(temp) > space_complexity:
-                space_complexity = len(temp)
-            queue.put((cost + city_map[current_city][neighbor], temp))
+    print("Time complexity: " + str(time_space))
+    print("Space complexity: " + str(time_space))
+    print("Path found: " + str(cities_list))
+    print("Cost = " + str(cost))
+    printMap(result)
